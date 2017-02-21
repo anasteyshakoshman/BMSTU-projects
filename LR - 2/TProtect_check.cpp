06fa8cb5f7741c9b7c163906c1a977f42afab111
Анастасия <nastena.andreevna.99@gmail.com>
2017-02-21 20:16:40 +0400

    Update TProtect_check.cpp

--- LR - 2/TProtect_check.cpp
+++ LR - 2/TProtect_check.cpp
@@ -20,8 +20,8 @@ int main()
 	}
 	std::cout << std::endl << "What type of elements will you want to see? Enter 0 - if type is asymmetric, 1 - if type is symmetric) : ";
 	std::cin >> check;
-	if (check == 1) std::cout <<std::endl << std::endl << "You chose symmetric type: " << std::endl << std::endl;
-	if (check == 0) std::cout << "You chose asymmetric type: " << std::endl << std::endl;
+	if (check == true) std::cout <<std::endl << std::endl << "You chose symmetric type: " << std::endl << std::endl;
+	if (check == false) std::cout << "You chose asymmetric type: " << std::endl << std::endl;
 	for (int i = 0; i < n; i++)
 	{
 		if (pS[i].GetType() == check) pS[i].print();
