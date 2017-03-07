87c759b4a0c828ddeaaceeb4161515e62cc086ea
Анастасия <nastena.andreevna.99@gmail.com>
2017-03-07 15:42:37 +0300

    Update Main.cpp

--- LR - 5/Main.cpp
+++ LR - 5/Main.cpp
@@ -7,7 +7,7 @@ void main()
 	TPair a;
 	std::cout << "Enter pair a" << std::endl;             //проверка метода ввода базового класса
 	std::cin >> a;                                        
-	TPair b(17, 25);
+	TPair b(8, 36);
 	std::cout << "\nPair b \n" << b;                      //проверка метода вывода базового класса
 
 	if (a == b) std::cout << "\na == b" << std::endl;       //проверка метода сравнения == базового класса
@@ -18,8 +18,9 @@ void main()
 	TFraction c;
 	std::cout << "\nEnter fraction c" << std::endl;         //проверка метода ввода производного класса
 	std::cin >> c;                                         
-	TFraction d(25, 100);
-	std::cout << "\nFraction d \n" << b;                  //проверка метода вывода производного класса
+	TFraction d;
+	std::cout << "\nEnter fraction d" << std::endl;         //проверка метода ввода производного класса
+	std::cin >> d;
 
 	if (c == d) std::cout << "\nc == d" << std::endl;       //проверка метода сравнения == производного класса
 	if (c > d) std::cout << "\nc > d" << std::endl;         //проверка метода сравнения > производного класса
