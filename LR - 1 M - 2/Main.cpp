c514f5accbc1659954cf2dda1c6bb49a06606795
Анастасия <nastena.andreevna.99@gmail.com>
2017-03-14 23:45:52 +0300

    Create Main.cpp

---
+++ LR - 1 M - 2/Main.cpp
@@ -0,0 +1,98 @@
+#include "Header.h"
+
+int main()
+{
+	try
+	{
+		boolFromString("hw");    //неизвестное значение
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+    
+	try
+	{
+		intFromString("2 147 483 647 999");          //превышения максимума
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+ 
+	try
+	{
+		intFromString("-2147483647999");      //превышение  предела минимума
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	try
+	{
+		  intFromString("аааааа");    //неизвестное значение
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	int num = 0;  
+	try
+	{
+		num = intFromString("-956");      //выведет -956
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << num << std::endl;
+
+
+	try
+	{
+		floatFromString("21474836479.9999");         //превышения максимума
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		floatFromString("-21474836479.9999");        //превышение  предела минимума
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		floatFromString("ffff");           //неизвестное значение
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	float number = 0;
+	try
+	{
+		number = floatFromString("-89,567");     //выведет -89,567
+	}
+	catch (TCastException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << number << std::endl;
+
+	system("pause");
+	return 0;
+}
