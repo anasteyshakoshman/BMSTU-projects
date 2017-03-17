ff96fac171e1e7e0005fe703f83e614e6169a10e
Анастасия <nastena.andreevna.99@gmail.com>
2017-03-18 03:53:02 +0800

    Create TInteger.h

---
+++ LR - 3 M - 2/TInteger.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <iostream>
+
+class TInteger
+{
+	int Num;
+
+public:   
+	TInteger();
+
+	TInteger(const int);      
+	TInteger(const long long int);      
+	TInteger& operator =(const long long int);     
+	TInteger& operator +=(const long long int);     
+	TInteger& operator *=(const long long int);     
+	TInteger& operator %=(const long long int);      
+	TInteger& operator /=(const long long int);
+	TInteger& operator -=(const long long int);
+	bool operator ==(const int) const;
+	bool operator <(const int) const;
+	bool Error(const long long int);
+
+	TInteger& operator ++();
+	TInteger& operator--();
+	TInteger(const TInteger&);             
+	TInteger& operator =(const TInteger&);
+	TInteger& operator +=(const TInteger&);
+	TInteger& operator *=(const TInteger&);
+	TInteger& operator %=(const TInteger&);
+	TInteger& operator /=(const TInteger&);
+	TInteger& operator -=(const TInteger&);
+	bool operator ==(const TInteger&) const;
+	bool operator <(const TInteger&) const;
+
+	friend std::ostream & operator<<(std::ostream & out, const TInteger &);
+};
+
+TInteger operator %(const TInteger&, const long long int);
+TInteger operator /(const TInteger&, const long long int);
+TInteger operator *(const TInteger&, const long long int);
+TInteger operator +(const TInteger&, const long long int);
+TInteger operator -(const TInteger&, const long long int);
+bool operator !=(const TInteger&, const long long int);
+bool operator >(const TInteger&, const long long int);
+
+TInteger operator %(const long long int, const TInteger&);
+TInteger operator /(const long long int, const TInteger&);
+TInteger operator *(const long long int, const TInteger&);
+TInteger operator +(const long long int, const TInteger&);
+TInteger operator -(const long long int, const TInteger&);
+bool operator !=(const long long int, const TInteger&);
+bool operator >(const long long int, const TInteger&);
+
+TInteger operator %(const TInteger&, const TInteger&);   
+TInteger operator /(const TInteger&, const TInteger&);
+TInteger operator *(const TInteger&, const TInteger&);
+TInteger operator +(const TInteger&, const TInteger&);
+TInteger operator -(const TInteger&, const TInteger&);
+bool operator !=(const TInteger&, const TInteger&);
+bool operator >(const TInteger&, const TInteger&);
