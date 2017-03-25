c3753eee5f54d23f411e29df9155f1d716a0cb2b
Анастасия <nastena.andreevna.99@gmail.com>
2017-03-25 10:10:53 +0300

    Create Header.h

---
+++ LR - 6 M - 2/Header.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+#include <exception>
+
+
+class TCastException : public std::exception
+{
+public:
+	TCastException() : exception()
+	{}
+	TCastException(const char * const & data) : exception(data)
+	{}
+};
+
+class TUnknown : public TCastException
+{
+public:
+	TUnknown() : TCastException()
+	{}
+	TUnknown(const char * const & data) : TCastException(data)
+	{}
+};
+
+class TMax : public TCastException
+{
+public:
+	TMax() : TCastException()
+	{}
+	TMax(const char * const & data) : TCastException(data)
+	{}
+};
+
+class TMin : public TCastException
+{
+public:
+	TMin() : TCastException()
+	{}
+	TMin(const char * const & data) : TCastException(data)
+	{}
+};
+
+
+
+//пponoтипы функций:
+template <typename T>
+T FromString(const char * data);
+template <>
+int FromString <int> (const char * data);
+template <>
+bool FromString <bool> (const char * data);
+template <>
+float FromString <float> (const char * data);
+
+
+
+
+
+
+
