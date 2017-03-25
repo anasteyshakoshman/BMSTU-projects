86c64e22a427ef934a3f55e3176f351700af9d94
Анастасия <nastena.andreevna.99@gmail.com>
2017-03-25 09:13:03 +0300

    Create Main.cpp

---
+++ LR - 7 M - 2/Main.cpp
@@ -0,0 +1,94 @@
+#include"TVector.h"
+#include <stdint.h>
+
+template <typename T>
+
+void printVector(const TVector <T> & vec)
+{
+	for (int i = 0; i < vec.size(); ++i)
+		std::cout << vec[i] << " ";
+	std::cout << std::endl;
+}
+
+int main()
+{
+	{
+		TVector <int> vec;
+		for (int i = 0; i < 32; ++i)
+			vec.push_back(i);
+
+		printVector(vec);
+
+		vec.insert(vec.begin() + 10, 1000);
+		printVector(vec);
+
+		vec.erase(vec.begin(), vec.begin() + 10);
+		printVector(vec);
+
+		vec.erase(vec.begin());
+		printVector(vec);
+
+		std::cout << vec.front() << std::endl;
+		std::cout << vec.back() << std::endl;
+
+		vec.pop_back();
+		std::cout << vec.back() << std::endl;
+
+		size_t oldSize = vec.size();
+		vec.resize(44);
+		printVector(vec);
+
+		vec.resize(oldSize);
+		printVector(vec);
+
+		TVector <int> vec2;
+		for (int i = 0; i < 2; ++i)
+			vec2.push_back(i);
+
+		vec2.swap(vec);
+		printVector(vec2);
+		printVector(vec);
+
+		vec2 = vec;
+		printVector(vec2);
+		printVector(vec);
+
+		try
+		{
+			TVector<int>::value_type item = vec2.at(1000000);
+		}
+		catch (const std::exception & e)
+		{
+			std::cout << e.what();
+		}
+
+		try
+		{
+		vec2.resize(UINT64_MAX);
+		}
+		catch (const std::exception & e)
+		{
+		std::cout << e.what() << std::endl;
+		}
+
+		TVector <double> vec3(16, 5.6);
+		printVector(vec3);
+
+		vec3.insert(vec3.end(), 22, 34.86786);
+		printVector(vec3);
+
+		TVector <char> vec3(10, 'W');
+		printVector(vec3);
+
+		vec3.insert(vec3.end(), 10, 'H');
+		printVector(vec3);
+
+		for (int i = 0; i < 10; ++i)
+		{
+			vec3.push_back('Y');
+		}
+		printVector(vec3);
+	}
+		system("pause");
+	}
+
