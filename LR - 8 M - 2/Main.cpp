0c46d8113bc2bd07074087f1e3d5982bd47356fc
Анастасия <nastena.andreevna.99@gmail.com>
2017-03-25 08:23:42 +0300

    Create Main.cpp

---
+++ LR - 8 M - 2/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+
+void swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <class IT>
+
+void sort(IT beg, IT end)
+{
+	for (IT i = beg; i != end; ++i)
+	{
+		for (IT k = i + 1; k != end; ++k)
+		{
+			if (*i > *k) swap(*i, *k);
+		}
+	}
+}
+
+
+int main()
+{
+	int arr[] = { 123, 3, 0, 13212, -100 };
+	sort(arr + 0, arr + 3); // arr: 0, 3, 123, 13212, -100
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+	sort(arr + 2, arr + 5); // arr: 0, 3, -100, 123, 13212
+	std::cout << std::endl;
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+	std::vector <double> vec = { 123.0, 3.0, 0.0, 13212.0, -100.0 };
+	sort(vec.begin(), vec.end()); // vec: -100.0, 0.0, 3.0, 123.0, 13212.0
+	for (int i = 0; i < 5; ++i)
+	{
+		std::cout << vec[i] << " ";
+	}
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
