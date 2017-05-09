7ca375989f66e700ab1b1acf3d1c5cb75a09487e
Анастасия <nastena.andreevna.99@gmail.com>
2017-05-10 02:11:16 +0300

    Update Flight.cpp

--- DZ - 2 - AIRPORT/Flight.cpp
+++ DZ - 2 - AIRPORT/Flight.cpp
@@ -1,13 +1,49 @@
 #include "Flight.h"
 
-Flight::Flight(const Airport & departure, const Airport &  destination)
+size_t Flight::Num = 0;
+
+
+Flight::Flight(Airport & departure, Airport &  destination)
 {
 	Departure = departure;
 	Destination = destination;
+	destination.SetFlight(*this);
+	departure.SetFlight(*this);
+	destination.PlusNumPlans();
+	departure.PlusNumPlans();
+	++Num;
 };
 
+Flight::Flight(std::initializer_list<Airport> & airport)    //{departure, destination}
+{
+	if (airport.size() > 2) throw std::length_error("Object 'Flight' can have just 2 parametres");
+	auto it = airport.begin();
+	Departure = *it;
+	++it;
+	Destination = *it;
+	++Num;
+};
+
+
 Flight::Flight()
-{};
+{
+	++Num;
+};
+
+size_t Flight::GetNum()
+{
+	return Num;
+};
+
+Airport  Flight::GetDeparture() const
+{
+	return Departure;
+};
+
+Airport  Flight::GetDestination() const
+{
+	return Destination;
+};
 
 Flight & Flight::operator =(const Flight & other)
 {
@@ -18,10 +54,12 @@ Flight & Flight::operator =(const Flight & other)
 };
 
 
+
 Flight::Flight(const Flight & other)
 {
 	Departure = other.Departure;
 	Destination = other.Destination;
+	++Num;
 };
 
 std::ostream & operator <<(std::ostream & out, const Flight & obj)
