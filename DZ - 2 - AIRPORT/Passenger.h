8f514812780a1012c560f48efa1a8e276eb36e52
Анастасия <nastena.andreevna.99@gmail.com>
2017-05-10 21:34:57 +0300

    Update Passenger.h

--- DZ - 2 - AIRPORT/Passenger.h
+++ DZ - 2 - AIRPORT/Passenger.h
@@ -9,6 +9,7 @@ private:
 
 	int Age;
 	std::string FIO;
+	std::string Location;
 	bool BigBaggage;
 	bool Pet;
 	Flight Voyage;
@@ -16,29 +17,19 @@ private:
 
 public:
 
-	Passenger(const std::string & fio, const Flight & voyage);
+	Passenger(const std::string & fio, Flight & voyage);
 
-	Passenger(const std::string & fio, const Flight & voyage, const int age, bool bigBaggage, bool pet);
+	Passenger(const std::string & fio, Flight & voyage, const int age, bool bigBaggage, bool pet);
 
-	Passenger & operator = (const Passenger & other);
+	Passenger & operator =(const Passenger & other);
 
 	friend std::ostream & operator <<(std::ostream & out, const  Passenger & obj);
 
-	int GetAge() const;
-
-	void SetAge(const int age);
-
 	std::string GetFIO() const;
-
-	void SetFIO(const std::string & fio);
 
 	bool GetBaggage() const;
 
 	void SetBaggage(const bool baggage);
-
-	bool GetPet() const;
-
-	void SetPet(const bool pet);
 
 	static size_t GetNum();
 };
