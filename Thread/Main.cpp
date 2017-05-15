f757ba0e57fbf2b5c4efa26ee87fc56d738b70f2
Анастасия <nastena.andreevna.99@gmail.com>
2017-05-15 10:41:07 +0300

    Update Main.cpp

--- Thread/Main.cpp
+++ Thread/Main.cpp
@@ -31,7 +31,7 @@ InfInt C(InfInt n, InfInt k)
 
 void main()
 {
-	InfInt n = 10, k = 6, c = C(n, k);
+	InfInt n = 1000, k = 10, c = C(n, k);   //263409560461970212832400
 	std::cout << c << std::endl;
 	system("pause");
 }
